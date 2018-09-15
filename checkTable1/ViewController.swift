788334bf29eb8bd5bb5fa3c10420ba8519c5374a
CodingApps <codingkit@gmail.com>
2018-09-15 03:07:54 -0700

    Initial Commit

---
+++ checkTable1/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  checkTable1
+//
+//  Created by Rick Mc on 9/14/18.
+//  Copyright © 2018 Rick Mc. All rights reserved.
+//
+
+    import UIKit
+    class MotoViewController : UITableViewController {
+        
+        var tableText = Array(repeating: "", count: 20)
+        var productList : [String] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+        var searchList : [String] = ["sony speakers", "samsung speakers", "dolby speakers", "lg speakers", "hitachi speakers", "panasonic speakers", "bose speakers", "sanyo speakers", "yamaha speakers", "pioneer speakers", "jbl speakers", "klipsch"]
+        
+        var currentRow : Int = 0
+        let textCellIndentifier = "itemCell"
+        
+
+        @IBOutlet var MotoTableView: UITableView!
+     
+        override func viewDidLoad() {
+      }
+        
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return productList.count
+        }
+        
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: textCellIndentifier, for: indexPath as IndexPath)
+            cell.textLabel?.text = productList[indexPath.row]
+            return cell
+        }
+        
+        func tableView(_ tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+                currentRow = indexPath.row
+                print("Text when selected.")
+                print(currentRow)
+        }
+}
+
---
+++ checkTable1/itemCell.swift
@@ -0,0 +1,15 @@
+//
+//  itemCell.swift
+//  checkTable1
+//
+//  Created by Rick Mc on 9/14/18.
+//  Copyright © 2018 Rick Mc. All rights reserved.
+//
+
+import UIKit
+
+internal class itemCell : UITableViewCell  {
+    
+    @IBOutlet weak var textLine: itemCell!
+    
+}
